f80bf7e528c0b4f9ea9554f76b2b65a645e7fcab
Steven Branson <steve@diodeware.com>
2019-03-19 10:17:35 -0500

    Issue #8 Setup EF Core Db Connection & Context

---
+++ FinalProjectServer/FinalProjectServer/Data/FinalProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinalProjectServer.Data
+{
+    public class FinalProjectContext : DbContext
+    {
+        // public DbSet<File> Files { get; set; }
+        // public DbSet<Folders> Folders { get; set; }
+
+        public FinalProjectContext(DbContextOptions<FinalProjectContext> options) : base(options) { }
+
+    }
+}

9a9383691aa1bc31db49d79fecb0e92414216129
naelmilien <nmilien09@gmail.com>
2019-03-20 14:46:11 -0500

    Setting up automapper

---
+++ FinalProjectServer/FinalProjectServer/AutoMapperFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FinalProjectServer.Data.Entities;
+using FinalProjectServer.Dtos;
+using File = AutoMapper.File;
+
+namespace FinalProjectServer
+{
+    public class AutoMapperFile : File
+    {
+        public AutoMapperFile()
+        {
+
+            CreateMap<FileResponseDto, File>();
+
+
+            CreateMap<FileUploadDto, File>();
+
+        }
+
+        
+    }
+}

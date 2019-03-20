8386c67701a08e35d603fe13b0f04a3b9aa38b75
LucasJenkins <ycayenne@gmail.com>
2019-03-20 14:46:31 -0500

    GetFilesByIds implemented in FilesServices

--- FinalProjectServer/FinalProjectServer/Data/FinalProjectContext.cs
+++ FinalProjectServer/FinalProjectServer/Data/FinalProjectContext.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using FinalProjectServer.Data.Entities;
 
 namespace FinalProjectServer.Data
 {
     public class FinalProjectContext : DbContext
     {
-        // public DbSet<File> Files { get; set; }
-        // public DbSet<Folders> Folders { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<Folder> Folders { get; set; }
 
         public FinalProjectContext(DbContextOptions<FinalProjectContext> options) : base(options) { }
 

430117c631073472c45f2582f5388488ee2a1352
naelmilien <nmilien09@gmail.com>
2019-03-20 14:03:41 -0500

    Downloaded automapper

--- FinalProjectServer/FinalProjectServer/AutoMapperFile.cs
+++ FinalProjectServer/FinalProjectServer/AutoMapperFile.cs
@@ -1,11 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DotnetAssessmentSocialMedia.Data.Entities;
+using DotnetAssessmentSocialMedia.Dtos;
+using FinalProjectServer.Data.Entities;
+using FinalProjectServer.Dtos;
+using Profile = AutoMapper.File;
 
-namespace FinalProjectServer
+namespace DotnetAssessmentSocialMedia
 {
-    public class AutoMapper
+    public class AutoMapperFile : File
     {
+        public AutoMapperFile()
+        {
+           
+
+            CreateMap<FileResponseDto, File>();
+                .ForMember(dest => dest.Credentials, opt => opt.MapFrom(src => src.Credentials))
+                .ForMember(dest => dest.Profile, opt => opt.MapFrom(src => src.Profile))
+                .ForAllOtherMembers(opts => opts.Ignore());
+
+            CreateMap<FileUploadDto, File>();
+
+
+            CreateMap<CreateUserDto, User>()
+                .ForMember(dest => dest.Credentials, opt => opt.MapFrom(src => src.Credentials))
+                .ForMember(dest => dest.Profile, opt => opt.MapFrom(src => src.Profile))
+                .ForAllOtherMembers(opts => opts.Ignore());
+
+            CreateMap<Credentials, CredentialsDto>();
+
+            CreateMap<Profile, ProfileDto>();
+
+            CreateMap<User, UserResponseDto>()
+                .ForMember(
+                    dest => dest.Username,
+                    opt => opt.MapFrom(src => src.Credentials.Username)
+                );
+
+
+        }
     }
 }

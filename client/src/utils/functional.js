a746fa2a205ecda64a2a7d36d4b94ec1ccb7ba1a
Christopher Gborgli <cagborgli@aol.com>
2019-03-22 11:47:15 -0500

    Removed local cached files.

--- client/src/utils/functional.js
+++
@@ -1,5 +0,0 @@
-export const delay = dispatch => ({
-  of: action => ({
-    by: ms => setTimeout(() => dispatch(action), ms)
-  })
-})
--- server/Server/Exception/CustomErrorResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace FinalProjectFileManager.Exception
-{
-    public class CustomErrorResponse
-    {
-        public string Message { get; set; }
-        public string Description { get; set; }
-    }
-}

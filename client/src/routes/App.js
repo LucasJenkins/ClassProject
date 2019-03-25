b2d80bfd2a10e4fddf2f5d92fc53231249e6ee41
Ryan Lunger <ryanlunger@gmail.com>
2019-03-25 14:48:04 -0500

    Issue #65: Asynchronous action for getting files.

--- client/src/routes/App.js
+++ client/src/routes/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import Home from '../views/Home'
-import Testing from '../containers/UploadTest'
+import Testing from '../views/Testing'
 import Trash from '../views/Trash'
 
 const App = props => (

6c8d856dda41de55e07e05f51d4e0589a6943ceb
Christopher Gborgli <cagborgli@aol.com>
2019-03-26 09:38:29 -0500

    deleting comments out of fileController

--- server/Server/Controllers/FileController.cs
+++ server/Server/Controllers/FileController.cs
@@ -67,22 +67,6 @@ namespace FinalProjectFileManager.Controllers
       return _mapper.Map<StorageItem, StorageItemResponseDto>(_fileService.GetById(id));
     }
 
-   /*  [HttpPatch("{id}/untrash")]
-    [ProducesResponseType(200)]
-    [ProducesResponseType(404)]
-    public ActionResult<StorageItemResponseDto> UntrashOne(int id)
-    {
-      return _mapper.Map<StorageItem, StorageItemResponseDto>(_fileService.UntrashItem(id));
-    }
-
-    [HttpPatch("untrash")]
-    [ProducesResponseType(200)]
-    [ProducesResponseType(404)]
-    public ActionResult<IEnumerable<StorageItemResponseDto>> Untrash([FromBody] IEnumerable<int> ids)
-    {
-      return _mapper.Map<IEnumerable<StorageItem>, IEnumerable<StorageItemResponseDto>>(_fileService.UntrashItems(ids)).ToList();
-    }*/
-
     [HttpPost]
     [RequestSizeLimit(100000000)]
     [ProducesResponseType(409)]
